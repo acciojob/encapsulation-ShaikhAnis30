952811c7c599396870084b6120d3a3bb38519487
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-01-11 14:59:49 +0530

    Encapsulation Assignment Completed

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name = "Java";
+
+    public void set(String newName) {
+        name = newName;
+    }
+
+    public String get(){
+        return name;
+    }
 }
