731c5373ff08c214fbbcef66e082dfd2a4853cf6
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-01-11 15:00:00 +0530

    Encapsulation Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "java Programming";
+        // Error -> 'name' has private access in 'com.exe.RWOnly'
+
+        obj.set("Java Programming");
+        String res = obj.get();
+        System.out.println(res);
+    }
 }
